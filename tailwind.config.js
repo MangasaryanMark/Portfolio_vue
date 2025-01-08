fdc5d3132e074c5059be8acf8df2e82932b63ea8
Mark <mangasaryanmark@gmail.com>
2025-01-08 14:40:50 +0400

    tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+import colors from 'tailwindcss/colors'
+module.exports = {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+
+    colors: {
+      ...colors,
+      'custom': '#6566e2',
+    },
+    fontFamily: {
+      sans: ['Graphic', 'sans-serif'],
+      serif: ['Merryweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
+  },
+  variants: {
+    extend: {
+      borderColor: ['focus-visible'],
+      opacity: ['disabled'],
+    }
+  }
+}
+
